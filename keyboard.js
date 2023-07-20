88096a9408cfa5c9f8615456f27a01412c1a0b17
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-20 16:26:00 +0330

    update style

--- keyboard.js
+++ keyboard.js
@@ -1,10 +1,19 @@
+const getTitle = document.querySelector('.title')
+
 document.addEventListener('keyup', function (event) {
+
+    showkeyUp(event)
+
     let keyUp = event.key
     let getKeyUp = document.getElementById(keyUp)
-    console.log(getKeyUp.id)
-    console.log(event)
     getKeyUp.classList.add('hit')
     getKeyUp.addEventListener('animationend', function () {
         getKeyUp.classList.remove('hit')
     })
 })
+
+
+function showkeyUp(event) {
+    console.log(event)
+    getTitle.innerHTML += event.key
+}
